f4287b05f1fbdb18a6fc83cf2f18affa5cd00fd7
VoffieDev <viktorsoderborg@gmail.com>
2024-07-31 22:17:58 +0200

    feat: New thread for each connection

--- lib/teapot/main.rb
+++ lib/teapot/main.rb
@@ -15,22 +15,35 @@ class Teapot
     @route_middlewares = []
   end
 
-  def listen(port, option)
-    server = TCPServer.new(port)
-    option.call
-    while (session = server.accept)
-      data = ''
-      while (line = session.gets) && line !~ /^\s*$/
-        data += line
+  def listen(setup_func)
+    setup_func.call
+
+    loop do
+      socket = @server.accept
+
+      Thread.new do
+        request = ''
+
+        while line = socket.gets and line !~ /^\s*$/
+          request += line
       end
-      parsed_data = parse(data)
-      if parsed_data[:Content_Length].to_i > 0
-        parsed_data[:body] = JSON.parse(session.read(parsed_data[:Content_Length].to_i))
-        parsed_data[:body] = parsed_data[:body].transform_keys(&:to_sym)
+
+        if request == ''
+          next
+        end
+
+        parsed_request = parse(request)
+
+        if parsed_request[:Content_Length].to_i > 0
+          parsed_request[:body] = JSON.parse(socket.read(parsed_request[:Content_Length].to_i))
+          parsed_request[:body] = parsed_request[:body].transform_keys(&:to_sym)
       end
-      response = @router.handle_method(parsed_data, @server_middlewares, @route_middlewares)
-      session.print response
-      session.close
+
+        response = handle_request(parsed_request)
+        
+        socket.print response
+        socket.close
+      end
     end
   end
 
