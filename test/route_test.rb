5af9c467442978fdab550d1ec6c9d9cb0989f974
VoffieDev <viktorsoderborg@gmail.com>
2024-08-02 16:17:50 +0200

    test: Add new test for refactor

--- test/route_test.rb
+++
@@ -1,6 +0,0 @@
-require 'minitest/autorun'
-describe "GET routes" do
-  it "Returns correct page for existing route" do
-    _(2+2).must_equal 4
-  end
-end
---
+++ test/utils/utils.rb
@@ -0,0 +1,21 @@
+require 'minitest/autorun'
+require_relative '../../lib/teapot/utils'
+
+include Utils
+
+describe "Regex parsing" do
+  it "Works with /" do
+    regex = generate_reg_exp("/")
+    assert_equal(regex.match("/").to_s, "/")
+  end
+
+  it "Works with multiple parts" do
+    regex = generate_reg_exp("/regex/test")
+    assert_equal(regex.match("/regex/test").to_s, "/regex/test")
+  end
+
+  it "Works with dynamic route" do
+    regex = generate_reg_exp("/params/:id")
+    assert_equal(regex.match("/params/52").to_s, "/params/52")
+  end
+end
