bfcb8ef94495a128a2eb0af06ca92970dd566582
voffie <viktorsoderborg@gmail.com>
2025-01-17 10:31:45 +0100

    refactor: clean up request parser

--- lib/teapot/parser.rb
+++ lib/teapot/parser.rb
@@ -5,22 +5,27 @@ VALID_METHODS = %w[GET POST PUT DELETE PATCH].freeze
 # Parser module to parse HTTP requests
 module Parser
   def parse(request)
-    request = request.split("\n")
-    return nil if request[0].nil?
+    lines = request.split("\n")
+    return nil if lines[0].nil?
 
-    method, resource, http = request[0].split
+    method, resource, http = lines[0].split
     parsed_request = {}
-    if VALID_METHODS.include? method
+
+    if VALID_METHODS.include?(method)
       parsed_request[:method] = method
       parsed_request[:resource] = resource
       parsed_request[:http] = http
-      request[1..].each do |row|
-        key, val = row.split(': ')
-        parsed_request[key.gsub('-', '_').to_sym] = val
+      lines[1..].each do |row|
+        key, val = row.split(': ', 2)
+        next if key.nil? || val.nil?
+
+        parsed_request[key.gsub('-', '_').to_sym] = val.strip
       end
     else
-      parsed_request[:error] = "Invalid method found in request! #{method} is not a valid method!"
+      parsed_request[:error] = "Invalid method: #{method}"
     end
     parsed_request
+  rescue StandardError => e
+    { error: "Error parsing request: #{e.message}" }
   end
 end
