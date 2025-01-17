2740e9412f416726745efadbc6808353bdbc1d9d
voffie <viktorsoderborg@gmail.com>
2025-01-17 10:39:46 +0100

    refactor: update path regex generation

--- lib/teapot/utils.rb
+++ lib/teapot/utils.rb
@@ -2,13 +2,11 @@
 
 # Module containing all utility methods required
 module Utils
-  # https://stackoverflow.com/questions/67407289/check-if-path-matches-dynamic-route-string
   def generate_reg_exp(path)
     return %r{^/$} if path == '/'
 
-    escape_dots = ->(s) { s.chars.each { |char| char == '.' ? '\\.' : char }.join }
-    regex = path.split('/').map { |s| s.start_with?(':') ? '[^\\/]+' : escape_dots.call(s) }
-    Regexp.new("^#{regex.join('\/')}$")
-  end
+    Regexp.new("^#{path.split('/').map do |segment|
+      segment.start_with?(':') ? '([^/]+)' : Regexp.escape(segment)
+    end.join('/')}$")
   end
 end
