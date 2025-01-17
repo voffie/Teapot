af5813795716ad79e45891bd604418b97c725af4
voffie <viktorsoderborg@gmail.com>
2025-01-17 10:38:58 +0100

    refactor: remove unused utils function

--- lib/teapot/utils.rb
+++ lib/teapot/utils.rb
@@ -10,15 +10,5 @@ module Utils
     regex = path.split('/').map { |s| s.start_with?(':') ? '[^\\/]+' : escape_dots.call(s) }
     Regexp.new("^#{regex.join('\/')}$")
   end
-
-  def get_params_from_path(path)
-    params = path.split('/').reject(&:empty?)
-    parsed_params = {}
-    unless params.empty? || params.none? { |param| param.start_with?(':') }
-      params.each do |param|
-        parsed_params[param] = ''
-      end
-    end
-    parsed_params
   end
 end
